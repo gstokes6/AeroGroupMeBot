9f9b502e893d6e84a1e309cf3c42597545db5a5d
Gavin Stokes <gstokes6@gmail.com>
2019-07-26 15:09:36 -0500

    Post On Run

--- Main.py
+++ Main.py
@@ -6,4 +6,6 @@ Token = os.getenv('GROUPME_BOT_ID')
 client = Client.from_token(Token)
 
 for group in client.groups.list_all():
-    print(gorup.name)
+    print(group.name)
+    message = group.post(text='hi')
+
