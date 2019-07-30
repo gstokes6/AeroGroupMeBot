320466b268e80b22ab6f4addb51616a2867a35de
Gavin Stokes <gstokes6@gmail.com>
2019-07-29 22:07:05 -0500

    Signed-off-by: Gavin Stokes <gstokes6@gmail.com>

--- Main.py
+++ Main.py
@@ -31,7 +31,7 @@ def webhook():
         for attachment in message['attachments']:
             if attachment['type'] == 'image':
                 TempURL = attachment['url']
-                FileName = message['created_at'] + '.' + TempURL.split('.')[-2]
+                FileName = str(message['created_at']) + '.' + TempURL.split('.')[-2]
                 tempfile = wget.download(TempURL,FileName)
                 print(tempfile)
                 GD.UploadFile(drive,tempfile,folder_id)
