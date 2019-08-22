bdee817b1a989ca0af63d5824e9bce44e066fcd4
Gavin Stokes <gstokes6@gmail.com>
2019-08-22 13:38:06 -0500

    Signed-off-by: Gavin Stokes <gstokes6@gmail.com>

--- Main.py
+++ Main.py
@@ -33,7 +33,7 @@ def webhook():
     # 'message' is an object that represents a single GroupMe message.
     message = request.get_json()
     print(message)
-    print(IsInClass(drive,message['created_at']))s
+    print(IsInClass(drive,message['created_at']))
     if (not sender_is_bot(message)) and (message['text']):
         message['text'] = message['text'].lower()
         CondenseResult,TypeResult,CommandType,attachment = san.Main(message['text'],message['attachments'])
