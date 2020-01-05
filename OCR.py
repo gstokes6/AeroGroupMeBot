48b8193913b13071ac249f9f6ba3cda232b61ec9
Gavin Stokes <gstokes6@gmail.com>
2020-01-04 21:18:23 -0600

    Signed-off-by: Gavin Stokes <gstokes6@gmail.com>

--- OCR.py
+++ OCR.py
@@ -6,8 +6,8 @@ import pytesseract
 from PIL import Image
 
 def main(Picture):
-    PictureFile = wget.download(Picture['url'],'OCRtemp.png')
-    image = Image.open('OCRtemp.png')
+    PictureFile = wget.download(Picture['url'],'OCRtemp.jpg')
+    image = Image.open('OCRtemp.jpg')
     Output =  pytesseract.image_to_string('OCRtemp.png', timeout=2)
     print(Output)
     return Output
