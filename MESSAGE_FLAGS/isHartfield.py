8fe114a0936395aead893cda557bdf2dd234c88a
gstokes6 <53351893+gstokes6@users.noreply.github.com>
2020-07-06 16:23:29 -0500

    Update isHartfield.py

--- MESSAGE_FLAGS/isHartfield.py
+++ MESSAGE_FLAGS/isHartfield.py
@@ -26,18 +26,18 @@ class isHartfield(messageFlag.messageFlag):
             self.isTrue = True
         
     def response(self):
-        GetHart(10)
+        self.GetHart(10)
         groupMe.reply_with_image("This is where I would put my hartfield code, IF I HAD ONE","ModifiedHart.jpg")
 
 
 ##Supporting Functions        
-    def TupleMulti(t1,t2):
+    def TupleMulti(self,t1,t2):
         t3 = []
         for i in range(0,len(t1)):
             t3.append(t1[i]*t2[i])
         return( round(t3[0]), round(t3[1]), round(t3[2]))
 
-    def colorize(im,weight):
+    def colorize(self,im,weight):
         """
         Colorize PIL image `original` with the given
         'red weight', 0-1; returns another PIL image.
@@ -47,22 +47,10 @@ class isHartfield(messageFlag.messageFlag):
         t2 = (1+weight*2,1-weight,1-weight)
         for X in range(0,normX):
             for Y in range(0,normY):
-                out.putpixel((X,Y),TupleMulti(im.getpixel((X,Y)),t2))
-        return out
-    def colorizeGreen(im,weight):
-        """
-        Colorize PIL image `original` with the given
-        'red weight', 0-1; returns another PIL image.
-        """
-        normX,normY = im.size
-        out = Image.new('RGB',(normX,normY))
-        t2 = (1-weight,1+weight*2,1-weight)
-        for X in range(0,normX):
-            for Y in range(0,normY):
-                out.putpixel((X,Y),TupleMulti(im.getpixel((X,Y)),t2))
+                out.putpixel((X,Y),self.TupleMulti(im.getpixel((X,Y)),t2))
         return out
 
-    def zoom(im,zoom):
+    def zoom(self,im,zoom):
         normX,normY = im.size
         scale = 1.0/zoom
         box = (round(normX/2) - round(normX*scale/2),round(normY/2) - round(normY*scale/2),round(normX/2) + round(normX*scale/2),round(normY/2) + round(normY*scale/2))
@@ -70,11 +58,10 @@ class isHartfield(messageFlag.messageFlag):
         out = out.resize((normX,normY))
         return out
 
-    def GetHart(i):
+    def GetHart(self,i):
         im = Image.open("centered-hartfield-roy.jpg")
         ZoomIntensity = 1+.15*i
         RedIntensity = .004*i*i
-        print(ZoomIntensity,RedIntensity)
-        z = zoom(im,ZoomIntensity)
-        out = colorize(z,RedIntensity)
+        z = self.zoom(im,ZoomIntensity)
+        out = self.colorize(z,RedIntensity)
         out.save('ModifiedHart.jpg')
