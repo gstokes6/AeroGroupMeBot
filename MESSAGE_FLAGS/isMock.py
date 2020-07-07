2f5a29c49a0b04d4eb1cea8c20351204693990f6
gstokes6 <53351893+gstokes6@users.noreply.github.com>
2020-07-06 19:33:47 -0500

    Update isMock.py

--- MESSAGE_FLAGS/isMock.py
+++ MESSAGE_FLAGS/isMock.py
@@ -12,18 +12,18 @@ class isMock(messageFlag.messageFlag):
         
     def checkTrue(self,message):
         ##Logic to find if flag is set
-        
+        self.message = message
         ##first see if invoked
         #invokedFlag = isMentioned.isMentioned(message,'73358488')#Gavin
         invokedFlag = isMentioned.isMentioned(message,'73362029')#Academic
         self.isTrue = invokedFlag.isTrue
         
     def response(self):
-        name = message['name']
-        nameID = message['sender_id']
-        loci = message['attachments'][0]['loci']
+        name = self.message['name']
+        nameID = self.message['sender_id']
+        loci = self.message['attachments'][0]['loci']
         print(loci)
-        newtext = self.Mock(message['text'].replace('@Gavin Stokes 2','@'+name))
+        newtext = self.Mock(self.message['text'].replace('@Gavin Stokes 2','@'+name))
         #newtext = Mock(message['text'].replace('@Academic','@'+name))
         replyMention(newtext,nameID,[loci[0],len(nameID)],bot_id)
 
