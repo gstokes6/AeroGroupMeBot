a826e79182ca149dfd1ac8f634aebb77d06710fa
gstokes6 <53351893+gstokes6@users.noreply.github.com>
2020-07-06 19:30:44 -0500

    fixes for the last time hopefully

--- MESSAGE_FLAGS/isMock.py
+++ MESSAGE_FLAGS/isMock.py
@@ -14,7 +14,8 @@ class isMock(messageFlag.messageFlag):
         ##Logic to find if flag is set
         
         ##first see if invoked
-        invokedFlag = isMentioned.isMentioned(message,'73358488')
+        #invokedFlag = isMentioned.isMentioned(message,'73358488')#Gavin
+        invokedFlag = isMentioned.isMentioned(message,'73362029')#Academic
         self.isTrue = invokedFlag.isTrue
         
     def response(self):
