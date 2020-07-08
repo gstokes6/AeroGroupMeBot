5e1c892462fb4766c85580bb2068ca009c6806b1
thetommycox <thetommycox@gmail.com>
2020-07-08 10:58:54 -0500

    re-nest and caps

--- MESSAGE_FLAGS/isSaturday.py
+++ MESSAGE_FLAGS/isSaturday.py
@@ -1,6 +1,8 @@
 from MESSAGE_FLAGS import messageFlag
 import groupMe
 
+## import datetime
+
 class isSaturday(messageFlag.messageFlag):
     def __init__(self,message):
         super().__init__(message)
@@ -9,15 +11,18 @@ class isSaturday(messageFlag.messageFlag):
     def checkTrue(self,message):
         ##Logic to find if flag is set
         splitText = message['text'].lower().split('saturdays',1)
+        ## weekDay = datetime.datetime.today().weekday()
         if ('saturdays' in message['text'].lower()) and ("dads" in splitText[1]):
             self.isTrue = True
-            if ("SAT" in message['scheduleList']):
+            ## if weekDay == 2:    ## Saturday
+            if ("SAT" in message['scheduleList'):
                 self.satCheck = True
             else:
                 self.satCheck = False
                 
         else:
             self.isTrue = False
+            self.satCheck = False
             
     def response(self):
         if self.satCheck:
