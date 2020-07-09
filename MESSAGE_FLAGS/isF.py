e8c4acea05574a6a053a1e0cf966e31dc9e313dc
thetommycox <thetommycox@gmail.com>
2020-07-08 20:14:54 -0500

    put that thing back where it came from
    or so help me

--- MESSAGE_FLAGS/isF.py
+++ MESSAGE_FLAGS/isF.py
@@ -7,9 +7,8 @@ class isF(messageFlag.messageFlag):
         
     def checkTrue(self,message):
         ##Logic to find if flag is set
-        splitF = message['text'].lower().split('f',1)
         Msg = message['text'].lower()
-        if(("f" in Msg) and ("get" in splitF[0])) or (("f" in Msg) and ("chat" in splitF[1])):
+        if("f in the chat" in Msg) or ("get an f" in Msg) or ("get a f" in Msg):
             self.isTrue = True
     def response(self):
         groupMe.reply('F')
