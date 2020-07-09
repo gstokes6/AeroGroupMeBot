2943ba6c13298c166d5d8ea7dea446dd81acaadc
thetommycox <thetommycox@gmail.com>
2020-07-08 19:55:23 -0500

    split f

--- MESSAGE_FLAGS/isF.py
+++ MESSAGE_FLAGS/isF.py
@@ -7,8 +7,9 @@ class isF(messageFlag.messageFlag):
         
     def checkTrue(self,message):
         ##Logic to find if flag is set
+        splitF = message['text'].lower().split('f',1)
         Msg = message['text'].lower()
-        if("f in the chat" in Msg) or ("get an f" in Msg) or ("get a f" in Msg):
+        if(("f" in Msg) and ("get" in splitF[0])) or (("f" in Msg) and ("chat") in splitF[1]):
             self.isTrue = True
     def response(self):
         groupMe.reply('F')
