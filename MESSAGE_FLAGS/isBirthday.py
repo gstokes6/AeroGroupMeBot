87f946006fa37a5f8336b5b0dee76f9f19b8cf1a
thetommycox <thetommycox@gmail.com>
2020-10-12 10:37:05 -0500

    bday test

---
+++ MESSAGE_FLAGS/isBirthday.py
@@ -0,0 +1,22 @@
+from MESSAGE_FLAGS import messageFlag
+import groupMe
+
+import datetime
+
+class isBirthday(messageFlag.messageFlag):
+    def __init__(self,message):
+        super().__init__(message)
+        self.retort = ""
+        
+    def checkTrue(self,message):
+        ##Logic to find if flag is set
+        currentDate = datetime.datetime.today()
+        if ('special day' in message['text'].lower()) and currentDate.day == 12 and currentDate.month == 10:
+            name = "Gavin Stokes"
+            self.isTrue = True
+                
+        else:
+            self.isTrue = False
+            
+    def response(self):
+        groupMe.reply_with_image('happy birthday to the absolute lad @'+name)
