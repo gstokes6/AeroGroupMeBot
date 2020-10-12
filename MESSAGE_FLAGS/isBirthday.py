e7bcb4359b9dce3c527a0a49b4d156ebaf582943
thetommycox <thetommycox@gmail.com>
2020-10-12 11:48:59 -0500

    COLONS

--- MESSAGE_FLAGS/isBirthday.py
+++ MESSAGE_FLAGS/isBirthday.py
@@ -11,14 +11,11 @@ class isBirthday(messageFlag.messageFlag):
     def checkTrue(self, message):
         ##Logic to find if flag is set
         currentDate = datetime.datetime.today()
-        if ('special' in message['text'].lower()) and ('day' in message['text'].lower())
+        if ('special' in message['text'].lower()) and ('day' in message['text'].lower()):
             self.isTrue = True
-            if (currentDate.day == 12) and (currentDate.month == 10):
-                name = 'Gavin Stokes'
-                chris = True
+            # if (currentDate.day == 12) and (currentDate.month == 10):
+            #    name = 'Gavin Stokes'
+            #    chris = True
             
     def response(self):
-        if self.isTrue == True:
-            groupMe.reply('happy birthday to the absolute lad @'+name)
-        if chris == True:
-            groupMe.reply('happy birthday to the absolute boy @'+name)
+            groupMe.reply('happy birthday to the absolute lad')# @'+name)
