802e4d9bd6b5e0be8e60290d97031b40b6b44430
gstokes6 <53351893+gstokes6@users.noreply.github.com>
2020-10-11 20:18:22 -0500

    adding markov

---
+++ MESSAGE_FLAGS/isMarkov.py
@@ -0,0 +1,44 @@
+from MESSAGE_FLAGS import messageFlag
+import markovify
+
+##for seeing if Gavin is invoked
+from MESSAGE_FLAGS import isMentioned
+import LOAD_ENV_VARS
+import groupMe
+
+
+class isMarkov(messageFlag.messageFlag):
+    def __init__(self,message):
+        super().__init__(message)
+        
+    def checkTrue(self,message):
+        ##Logic to find if flag is set
+        self.message = message
+        ##first see if invoked
+        #invokedFlag = isMentioned.isMentioned(message,'73358488')#Gavin
+        invokedFlag = isMentioned.isMentioned(message,'73362029')#Academic
+        isInvoked = invokedFlag.isTrue
+
+        ##see if keyword is in message
+        Msg = message['text'].lower()
+        Msg = Msg.replace('@academic','').replace('[[academic]]','')
+        isKeyPhrase = ( "markov" in Msg )
+
+        if (isKeyPhrase and isInvoked):
+            self.willLike = True
+            self.isTrue = True
+        
+    def response(self):
+        nameID = self.message['sender_id']
+        groupMe.reply(Markov(nameID))
+
+    def Markov(sender_id):
+        path = 'json_models/' + sender_id + '.json'
+        with open(path,'r',encoding="utf-8") as f:
+            text = f.read()  
+        reconstituted_model = markovify.Text.from_json(model_json)
+
+        response = ''
+        for i in range(5):
+            response = response + reconstituted_model.make_sentence() + '\n'
+        return response
